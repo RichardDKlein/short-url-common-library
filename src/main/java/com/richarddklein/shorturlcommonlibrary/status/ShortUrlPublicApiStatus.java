8bf81ab400731a83a5acccb3cfc1280a21c7c19e
RichardDKlein <RichardDKlein@gmail.com>
2024-10-15 15:36:49 -0500

    Add ShortUrlPublicApiStatus enum

---
+++ src/main/java/com/richarddklein/shorturlcommonlibrary/status/ShortUrlPublicApiStatus.java
@@ -0,0 +1,11 @@
+/**
+ * The Short URL Common Library
+ * (Copyright 2024 by Richard Klein)
+ */
+
+package com.richarddklein.shorturlcommonlibrary.status;
+
+public enum ShortUrlPublicApiStatus {
+    SUCCESS,
+    UNKNOWN_ERROR,
+}
